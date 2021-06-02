dd8b1b5fc966c9af8a054b0cf2f4255f5e4b5851
vincentvanp <52759139+vincentvanp@users.noreply.github.com>
2021-06-02 13:07:09 +0200

    Removed everything except build

--- src/js/script.js
+++
@@ -1,8 +0,0 @@
-function navFunction() {
-    var x = document.getElementById("navListId");
-    if (x.className === "navlist") {
-        x.className += " responsive";
-    } else {
-        x.className = "navlist";
-    }
-}
